c1f609d823c3ba41906146442357dfbe65c97500
cheesiang89 <leecheesiang@gmail.com>
2018-01-16 22:30:59 +0800

    Added EF model, pulled table to Clerk->Inventiry page

--- Team10AD_Web/Clerk/Inventory.aspx
+++ Team10AD_Web/Clerk/Inventory.aspx
@@ -3,7 +3,8 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
     <h2>Inventory    </h2>
-    <asp:GridView ID="GridView1" runat="server">
+    <asp:GridView ID="dgvCatalogue" runat="server">
         </asp:GridView>
 
+ 
 </asp:Content>
--- Team10AD_Web/Clerk/Inventory.aspx.cs
+++ Team10AD_Web/Clerk/Inventory.aspx.cs
@@ -5,14 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Team10AD_Web;
+using Team10AD_Web.App_Code.Model;
 
 namespace Team10AD_Web.Clerk
 {
     public partial class Inventory : System.Web.UI.Page
     {
-     
+        private Team10ADModel m = new Team10ADModel();
         protected void Page_Load(object sender, EventArgs e)
         {
+            dgvCatalogue.DataSource = m.Catalogues.Select(
+                x => new {
+                    ItemCode = x.ItemCode,
+                Category=x.Category,
+                Description=x.Description}).ToList();
+            dgvCatalogue.DataBind();
+           
         }
     }
 }
