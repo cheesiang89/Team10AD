1d5bce557c574dd071edec7b40ce447c8c52f6dc
Raymond Tay <raymond_txz@hotmail.com>
2018-01-20 18:43:33 +0800

    Updated retrieval pages and linked up requisition with retrieval

---
+++ Team10AD_Web/Clerk/RetrievalDetailPage.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Clerk/Clerk.Master" AutoEventWireup="true" CodeBehind="RetrievalDetailPage.aspx.cs" Inherits="Team10AD_Web.Clerk.RetrievalDetailPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
--- Team10AD_Web/Clerk/RetrievalList.aspx.cs
+++ Team10AD_Web/Clerk/RetrievalList.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Team10AD_Web.App_Code.Model;
+using Team10AD_Web.App_Code;
 
 namespace Team10AD_Web.Clerk
 {
@@ -11,7 +13,30 @@ namespace Team10AD_Web.Clerk
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                dgvRetList.DataSource = RayBizLogic.RetrievalListForGV();
+                dgvRetList.DataBind();
+                dgvRetList.AllowPaging = true;
+            }
+        }
 
+        protected void dgvRetList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Details")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow selectedRow = dgvRetList.Rows[index];
+                Session["retrievaldetail"] = selectedRow.Cells[0].Text;
+                Response.Redirect("RetrievalDetailPage.aspx");
+            }
+        }
+
+        protected void dgvRetList_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            dgvRetList.PageIndex = e.NewPageIndex;
+            dgvRetList.DataSource = RayBizLogic.RetrievalListForGV();
+            dgvRetList.DataBind();
         }
     }
 }
