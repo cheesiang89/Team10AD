d126105158eb39adeb22567fccfa1ea11b2b97b4
cheesiang89 <leecheesiang@gmail.com>
2018-01-23 10:46:47 +0800

    Added email utility on Adjustment

--- Team10AD_Web/EmployeePage/Test.aspx.designer.cs
+++ Team10AD_Web/EmployeePage/Test.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace Team10AD_Web.EmployeePage {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnAutoEmail;
+        
+        /// <summary>
+        /// CrystalReportViewer1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::CrystalDecisions.Web.CrystalReportViewer CrystalReportViewer1;
     }
 }
